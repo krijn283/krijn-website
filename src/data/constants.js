55975e432e33d11446302edf6d0a965d58716764
krijn283 <157394927+krijn283@users.noreply.github.com>
2024-11-07 11:32:09 +0100

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,284 @@
+export const Bio = {
+  name: "Krijn van Lankveld",
+  roles: [
+    "Student ORM",
+    "19 jaar oud",
+    "Wonend in Breda",
+    "Sociaal en Enthousiast",
+  ],
+  description:
+    "Ik ben een gemotiveerde en veelzijdige 19-jarige student aan Hogeschool Avans Breda, altijd enthousiast om nieuwe uitdagingen aan te gaan. Sinds jonge leeftijd ben ik geïnteresseerd in technologie en sociale media, wat me heeft geïnspireerd om mijn kennis op deze gebieden verder uit te breiden..",
+  github: "https://www.linkedin.com/in/krijn-van-lankveld-786b55256/?originalSubdomain=nl",
+  resume:
+    "https://drive.google.com/file/d/1wYhYohn0szFE7--eKLwwl7v9Z0WPptcV/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/krijn-van-lankveld-786b55256/?originalSubdomain=nl",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com//",
+  facebook: "https://www.facebook.com/",
+};
+
+export const skills = [
+  {
+    title: "Social Media",
+    skills: [
+      {
+        name: "44,900 actieve volgers behaald doormiddel van organisch posten",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg",
+      },
+      {
+        name: "11,400 actieve volgers behaald doormiddel van organisch posten",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg",
+      },
+      {
+        name: "33,700 actieve volgers behaald doormiddel van organisch posten",
+        image:
+          "https://www.pagetraffic.com/blog/wp-content/uploads/2022/06/tiktok-logo-black.png",
+      },
+    ],
+  },
+  {
+    title: "Video-editing",
+    skills: [
+      {
+        name: "Half Jaar ervaring met Adobe Premiere Pro",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/40/Adobe_Premiere_Pro_CC_icon.svg",
+      },
+      {
+        name: "Twee Jaar ervaring met Capcut",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/a0/Capcut-logo.svg",
+      },
+      {
+        name: "Verschillende Freelancer Video-edit Jobs",
+        image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Fiverr_Logo_09.2020.svg",
+      },
+      {
+        name: "Eigen Video's geëdit voor mijn Kanalen",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg",
+      },
+    ],
+  },
+  {
+    title: "Foto-editing",
+    skills: [
+      {
+        name: "Twee Jaar ervaring met Canva",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/08/Canva_icon_2021.svg",
+      },
+      {
+        name: "Half Jaar ervaring met Adobe Photoshop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg",
+      },
+      {
+        name: "Verschillende Freelancer Foto-edit Jobs",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/Fiverr_Logo_09.2020.svg",
+      },
+      {
+        name: "Eigen Foto's geëdit voor mijn Kanalen",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg",
+      },
+    ],
+  },
+  {
+    title: "Graphic Design",
+    skills: [
+      {
+        name: "Half jaar ervaring AI Illustrator",
+        image:
+          "https://cdn.pixabay.com/photo/2021/02/03/12/28/adobe-illustrator-5977785_1280.png",
+      },
+      {
+        name: "Twee Jaar ervaring met Canva",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/08/Canva_icon_2021.svg",
+      },
+      {
+        name: "Graphic Design Events bijgewoond",
+        image:
+          "https://pbs.twimg.com/profile_images/647518695854632960/rI8AiCiX_400x400.jpg",
+      },
+      {
+        name: "Verschillende Graphic Design Jobs",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/18/Fiverr_Logo_09.2020.svg",
+      },
+    ],
+  },
+  {
+    title: "Websites bouwen",
+    skills: [
+      {
+        name: "Drie Jaar ervaring met Wordpress",
+        image:
+          "https://static-00.iconduck.com/assets.00/wordpress-icon-2048x2048-nw66awj8.png",
+      },
+      {
+        name: "Twee Jaar ervaring met HTML",
+        image:
+          "https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png",
+      },
+      {
+        name: "Twee Jaar ervaring met CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+      },
+      {
+        name: "Half Jaar ervaring met Javascript",
+        image:
+          "https://1000logos.net/wp-content/uploads/2020/09/JavaScript-Logo.png",
+      },
+    ],
+  },
+  {
+    title: "Python",
+    skills: [
+      {
+        name: "Beheersing van Python",
+        image:
+          "https://image.similarpng.com/very-thumbnail/2021/12/Python-programming-logo-on-transparent-background-PNG.png",
+      },
+      {
+        name: "Beheersing van Selenium",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Selenium_Logo.png",
+      },
+      {
+        name: "Beheersing van Beautifulsoup",
+        image:
+          "https://datascientest.com/en/files/2024/01/beautiful-soup.png",
+      },
+      {
+        name: "Beheersing van VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://banner2.cleanpng.com/20180619/uoi/aa68fvxn5.webp",
+    role: "Vakkenvuller",
+    company: "Jumbo Zuiderhout Oosterhout",
+    date: "Dec 2021 - Nov 2022",
+    desc: "Tijdens mijn tijd als vakkenvuller bij de Jumbo heb ik waardevolle ervaring opgedaan in klantgerichtheid en teamwork. Ik zorgde voor een nette en overzichtelijke winkel, wat bijdroeg aan een positieve winkelervaring voor klanten.",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_TpaQaGIiAapFEmNIGzlLZO_V69tyVi4x6A&s",
+    role: "Hospitality Medewerker",
+    company: "HP Events",
+    date: "Feb 2023 - Sep 2023",
+    desc: "Als barmedewerker bij HP Events heb ik gewerkt op grote evenementen, waaronder wedstrijden van Feyenoord en NAC, evenals festivals zoals Dominator en Defqon. In deze rol was ik verantwoordelijk voor het bedienen van bezoekers, het voorbereiden van drankbestellingen en het zorgen voor een vlotte en vriendelijke service in een drukke omgeving. Deze ervaring heeft mijn vaardigheden in klantcontact en teamwork versterkt, terwijl ik leerde om effectief om te gaan met drukte en diverse situaties.",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuAyyftHCYL02j-xyDjO-8zCpFZaDgIvdemQ&s",
+    role: "Bediening Horeca",
+    company: "Spa One",
+    date: "Juli 2023 - Apr 2024",
+    desc: "Tijdens mijn werk bij Spa One was ik verantwoordelijk voor het bieden van een onvergetelijke spa-ervaring aan onze gasten. In deze rol verzorgde ik de bediening en zorgde ik ervoor dat de gasten zich volledig ontspannen voelden. Ik gaf attent advies over behandelingen en producten, en zorgde voor een vriendelijke en professionele service, waardoor elke gast het gevoel had dat hun comfort en welzijn voorop stonden.",
+   },
+  {
+    id: 3,
+    img: "https://www.handleidi.ng/thumbs/brands/s/271-ferm_logo.jpg",
+    role: "Stagiare Product Research",
+    company: "Ferm International",
+    date: "mrt 2023 - Jun 2023",
+    desc: "Tijdens mijn stage bij Ferm International was ik actief op de afdeling product research. Mijn werkzaamheden omvatten het onderzoeken en identificeren van nieuwe productmogelijkheden voor het bedrijf. Ik heb marktonderzoek uitgevoerd, trends geanalyseerd en potentiële producten geëvalueerd, wat heeft bijgedragen aan de ontwikkeling van ons assortiment. Deze ervaring heeft mijn onderzoeks- en analytische vaardigheden verder versterkt en me inzicht gegeven in de productontwikkeling binnen de industrie.",
+  },
+  {
+    id: 4,
+    img: "https://play-lh.googleusercontent.com/yKeAASYDY_HycXVATAu5yqZjhQHnArSm-bqYXFpjGNH4tPwODQUnsGKCnRGhurGSUhI=w600-h300-pc0xffffff-pd",
+    role: "",
+    company: "'T Houtse Frituurtje",
+    date: "Mrt 2024 - Nu",
+    desc: "Momenteel ben ik werkzaam bij 'T Houtse Frituurtje, een populaire cafetaria in Oosterhout. In mijn rol ben ik verantwoordelijk voor het openen en sluiten van de cafetaria, wat inhoudt dat ik zorg draag voor een goede start en afsluiting van de dagelijkse werkzaamheden. Ik werk gemiddeld 4 à 5 dagen per week, waar ik niet alleen klanten bedien, maar ook zorg voor de algehele organisatie en netheid van de zaak. Deze ervaring heeft mijn verantwoordelijkheidsgevoel en klantenservicevaardigheden verder ontwikkeld.",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiPN_PVmh6KYfTbhnEHd6A2rtdUQEVC-0gcQ&s",
+    school: "Mgr Frencken College",
+    date: "Aug 2017 - Jul 2022",
+    grade: "6.3",
+    desc: "Ik heb mijn Havo-diploma behaald aan het Mgr. Frencken College, een proces dat ik in een periode van vijf jaar heb voltooid. Gedurende deze tijd heb ik niet alleen vakkennis opgedaan, maar ook belangrijke vaardigheden ontwikkeld die me hebben voorbereid op verdere studies en mijn toekomstige carrière. Deze ervaring heeft bijgedragen aan mijn persoonlijke en academische groei.",
+    degree: "Havo Diploma",
+  },
+  {
+    id: 1,
+    img: "https://geoinformatienederland.nl/app/uploads/2022/08/download-3-1.png",
+    school: "Hogeschool Avans Breda",
+    date: "Sep 2021 - Jun 2022",
+    desc: "vormt een solide basis voor mijn verdere studie en bevestigt mijn inzet en motivatie binnen dit vakgebied. Tijdens dit jaar heb ik waardevolle kennis opgedaan over de fundamenten van ondernemerschap, marketing en retail, en ben ik enthousiast om mijn kennis en vaardigheden verder uit te breiden in de komende jaren.",
+    degree: "Propedeuse Ondernemerschap & Retail Management",
+  },
+  {
+    id: 2,
+    img: "https://geoinformatienederland.nl/app/uploads/2022/08/download-3-1.png",
+    school: "Hogeschool Avans Breda",
+    date: "Sep 2021 - Heden",
+    desc: "Ik volg momenteel de hbo-opleiding Ondernemerschap & Retail Management. Deze studie geeft me de kans om zowel mijn ondernemende vaardigheden als mijn kennis van de retailsector verder te ontwikkelen. Tijdens de opleiding leer ik alles over bedrijfsstrategie, marketing, verkoop, en het opzetten en managen van een eigen onderneming. Dit past perfect bij mijn ambitie om in de toekomst succesvol te worden in de wereld van ondernemerschap en retail.",
+    degree: "HBO Ondernemerschap & Retail Management",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Groenverwarmd",
+    date: "Nov 2021 - Dec 2021",
+    description:
+      "Een webshop gespecialiseerd in innovatieve zonnepanelen, met een breed assortiment van de nieuwste energie-efficiënte oplossingen. De webshop biedt gedetailleerde productinformatie en advies op maat, zodat klanten eenvoudig de beste keuze kunnen maken voor duurzame energieoplossingen.",
+    image:
+      "https://raw.githubusercontent.com/krijn17/Krijncv/refs/heads/main/Scherm%C2%ADafbeelding%202024-11-05%20om%203.08.13%20PM.png",
+    tags: [
+      "React Js",
+      "PostgressSQL",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "React Flow",
+    ],
+    category: "web app",
+    github: "https://www.groenverwarmd.nl/",
+    webapp: "https://www.groenverwarmd.nl/",
+  },
+  {
+    id: 9,
+    title: "HpCarCare",
+    date: "Jun 2022 - Jul 2022",
+    description:
+      "Een gebruiksvriendelijke website waar je snel en eenvoudig een afspraak kunt maken om je auto professioneel te laten schoonmaken. Bezoekers kunnen direct een schoonmaakpakket kiezen, de gewenste datum en tijd selecteren en online boeken. De website biedt duidelijk overzicht van de diensten, tarieven, en geeft handige tips voor autoverzorging. Ideaal voor wie zijn auto gemakkelijk wil laten stralen!",
+    image:
+      "https://raw.githubusercontent.com/krijn17/Krijncv/refs/heads/main/Scherm%C2%ADafbeelding%202024-11-05%20om%203.15.38%20PM.png",
+    category: "web app",
+    github: "https://www.hpcarcare.nl/",
+    webapp: "https://www.hpcarcare.nl/",
+  },
+  {
+    id: 0,
+    title: "FairJobs Prototype App",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "In opdracht van Fairjobs heb ik een app ontwikkeld die gericht is op het bevorderen van duurzame werkgelegenheid. De app biedt gebruikers de mogelijkheid om eenvoudig vacatures te vinden die aansluiten bij hun vaardigheden en interesses, met een focus op eerlijke en verantwoorde werkplekken. Tijdens het ontwikkelproces heb ik nauw samengewerkt met het team van Fairjobs om ervoor te zorgen dat de functionaliteiten en de gebruikerservaring aan hun wensen en behoeften voldoen.",
+    image:
+      "https://raw.githubusercontent.com/krijn17/Krijncv/refs/heads/main/Scherm%C2%ADafbeelding%202024-11-05%20om%203.35.30%20PM.png",
+    category: "android app",
+    github: "https://previewer.adalo.com/194344bc-47b2-4963-8e9f-533b84c4bed6?_gl=1*17vtyyj*_gcl_au*OTM4ODY1Mjg0LjE3MzA4MTcyNjAuMzk2MTA4NzIyLjE3MzA4MTcyNjUuMTczMDgxNzI2NQ..*_ga*MTYzMjgyMjAxMy4xNzMwODE3MjYw*_ga_SWT45DV35L*MTczMDgxNzI1OS4xLjEuMTczMDgxNzMyMC41OS4wLjA.",
+    webapp: "https://previewer.adalo.com/194344bc-47b2-4963-8e9f-533b84c4bed6?_gl=1*17vtyyj*_gcl_au*OTM4ODY1Mjg0LjE3MzA4MTcyNjAuMzk2MTA4NzIyLjE3MzA4MTcyNjUuMTczMDgxNzI2NQ..*_ga*MTYzMjgyMjAxMy4xNzMwODE3MjYw*_ga_SWT45DV35L*MTczMDgxNzI1OS4xLjEuMTczMDgxNzMyMC41OS4wLjA.",
+  },
+];
+
